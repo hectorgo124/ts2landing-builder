0c9f62f30f63e410da998ee55de65966fcf3e71c
hectorgo124 <hgranelloliva@gmail.com>
2024-06-12 22:30:05 +0200

    Add copy to clipboard function on Code Modal

--- src/app/code-modal/code-modal.component.ts
+++ src/app/code-modal/code-modal.component.ts
@@ -9,15 +9,31 @@ import { LandingState } from '../state/landing/landing.state';
   standalone: true,
   imports: [StringifyPipe],
   templateUrl: './code-modal.component.html',
-  styleUrl: './code-modal.component.scss'
+  styleUrl: './code-modal.component.scss',
 })
 export class CodeModalComponent {
   landing = this.store.selectSignal(LandingState.getLanding);
 
+  isOpen = false;
+  showAlert = false;
+
   constructor(private store: Store) {}
 
-  isOpen = false;
-
+  copyJsonToClipboard() {
+    const jsonStr = JSON.stringify(this.landing(), null, 2);
+    navigator.clipboard
+      .writeText(jsonStr)
+      .then(() => {
+        this.showAlert = true;
+        setTimeout(() => {
+          this.showAlert = false;
+        }, 2000); // Ocultar aviso después de 2 segundos
+      })
+      .catch((err) => {
+        console.error('Failed to copy JSON to clipboard: ', err);
+      });
+  }
+  
   openModal() {
     this.isOpen = true;
   }
